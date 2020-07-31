f952f4485770aa39851e57d5b019cd11e8bf8c3c
pedrocaximiliano <pedro_caximiliano@hotmail.com>
2020-07-31 03:11:41 -0300

    ajuste no dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,60 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import { StackedAreaChart, Grid, YAxis, ProgressCircle, PieChart } from 'react-native-svg-charts'
+import { StackedAreaChart, Grid, YAxis, ProgressCircle, PieChart, AreaChart, LineChart } from 'react-native-svg-charts'
 
-import { View, ActivityIndicator, StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
+import { View, ActivityIndicator, StyleSheet, Text, SafeAreaView, ScrollView, Image } from 'react-native';
 
-
-import * as shape from 'd3-shape'
+import { Text as TextCharts } from 'react-native-svg';
 export default function dashboard() {
 
     const [loading, setLoading] = useState(true);
-    useEffect(() => {
 
-        // chamar a API
-        setTimeout(function () {
-            setLoading(false);
-        }, 3000);
+    const [deathPercentageWOW, setDeathPercentageWOW] = useState(0); 
+
+    const [totalPopulation, setTotalPopulation] = useState(0); 
+
+    const [confirmed, setConfirmed] = useState([]);
+
+    const [deaths, setDeaths] = useState([]); 
+
+    const [teste, setTeste] = useState([]); 
+
+    const [teste1, setTeste1] = useState([]); 
+
+    const [confirmedPercentage, setConfirmedPercentage] = useState([]);
+
+
+    useEffect(() => {
+        try {
+            fetch(`https://api.weather.com/v3/wx/disease/tracker/country/60day?format=json&geocode=-22.91216,-43.17501&apiKey=d522aa97197fd864d36b418f39ebb323`
+            ).then(
+                response => response.json()
+            ).then(
+                data => (
+                    setDeathPercentageWOW(data.covid19.deathPercentageWOW),
+                    setTotalPopulation(data.covid19.totalPopulation),
+                    setConfirmedPercentage(data.covid19.confirmedPercentageWOW),
+                    setDeaths(data.covid19.deaths),
+                    setConfirmed(data.covid19.confirmed),
+                    setLoading(false)
+                ),
+            )
+        } catch (error) {
+            alert('erro, verifique a conexão com a internet');
+        }
     }, [])
 
+    useEffect(() => {
+        setTeste(confirmed.reverse());
+        setTeste1(deaths.reverse());
+        
 
-     // recebe um array para montar o grafico pieCharts
-    const data1 = [50, 10, 40, 95];
 
-    const piechart = data1.map((value, index) => ({
-        value,
-        key: `${index}` - `${value}`,
-        svg: {
-            fill: '#2A56C6',
-        }
-    }));
+    }, [confirmed])
 
-    // para o graáfico de risco e necessario passar um decimal, acredito que esse valor vem da API.
 
-    // recebe o objeto para montar o grafico de infectados do mês
-    const data = [
-        {
-            month: new Date(2020, 2, 1),
-            infects: 1600,
-            testing: 1440,
+    // const data1 = [500000, 100000, 400000, 950000];
 
-        },
-        {
-            month: new Date(2020, 2, 1),
-            infects: 3000,
-            testing: 960,
+    // const piechart = data1.map((value, index) => ({
+    //     value,
+    //     key: `${index}` - `${value}`,
+    //     svg: {
+    //         fill: 'red',
+    //     }
+    // }));
 
-        },
-        {
-            month: new Date(2020, 2, 1),
-            infects: 5000,
-            testing: 900,
+    // const Label = ({ slices }) => {
+    //     return slices.map((slice, index) => {
+    //         const { pieCentroid, data } = slice;
+    //         return (
+    //             <TextCharts
+    //                 key={`label-${index}`}
+    //                 x={pieCentroid[0]}
+    //                 y={pieCentroid[1]}
+    //                 fill="black"
+    //                 textAnchor={'middle'}
+    //                 aligmentBaseline={'middle'}
+    //             >
+    //                 {data.value}
+    //             </TextCharts >
+    //         )
 
-        },
-    ]
+    //     })
+    // }
 
-    const colors = ['#cc66ff', '#eeccff']
-    const keys = ['infects', 'testing']
     return (
+
         loading ? (
             <View style={[styles.container, styles.horizontal]}>
                 <ActivityIndicator size="large" animating={loading} color="#2A56C6" />
@@ -63,58 +91,87 @@ export default function dashboard() {
                 <>
                     <SafeAreaView>
                         <ScrollView>
+                            <View>
+                                <Text>
+                                    População total no brasil {totalPopulation}
+                                </Text>
+
+                            </View>
                             <View style={styles.areChartsContainer}>
-                                <Text>Gráfico dde aumento de infectados por mês</Text>
+                                <Text>confirmações em 60 dias {confirmed[0]} mil</Text>
                                 <View style={styles.areChartsView}>
-                                    <StackedAreaChart
-                                        style={{ flex: 1 }}
-                                        contentInset={{ top: 10, bottom: 10 }}
-                                        data={data}
-                                        keys={keys}
-                                        colors={colors}
-                                        curve={shape.curveNatural}
+                                    <YAxis
+                                        data={teste}
+                                        svg={{
+                                            fill: 'grey',
+                                            fontSize: 10,
+                                        }}
+                                        numberOfTicks={10}
+                                    />
+                                    <LineChart
+                                        style={{ flex: 1, marginLeft: 16 }}
+                                        data={teste}
+                                        svg={{ stroke: 'rgb(134, 65, 244)' }}
                                     >
                                         <Grid />
-                                    </StackedAreaChart>
-                                    <YAxis
-                                        style={{ position: 'absolute', top: 0, bottom: 0 }}
-                                        data={StackedAreaChart.extractDataPoints(data, keys)}
-                                        contentInset={{ top: 10, bottom: 10 }}
-                                        svg={{
-                                            fontSize: 10,
-                                            fill: 'black',
-                                            stroke: 'black',
-                                            strokeWidth: 0.1,
-                                            alignmentBaseline: 'baseline',
-                                            baselineShift: '3',
-                                        }}
-                                    />
+                                    </LineChart>
                                 </View>
+                                <Text>Mortes em 60 dias {deaths[0]} mil</Text>
+                                <View style={styles.areChartsView}>
+                                    <YAxis
+                                        data={teste1}
+                                        svg={{
+                                            fill: 'grey',
+                                            fontSize: 10,
+                                        }}
+                                        numberOfTicks={10}
+                                    />
+                                    <LineChart
+                                        style={{ flex: 1, marginLeft: 16 }}
+                                        data={teste1}
+                                        svg={{ stroke: 'rgb(134, 65, 244)' }}
+                                    >
+                                        <Grid />
+                                    </LineChart>
+                                </View>
+
                             </View>
                             <View style={styles.graficContainer}>
                                 <View style={styles.graficView}>
-                                <Text style={styles.text}>grafico</Text>
+                                    <Text style={styles.text}>mortes %</Text>
                                     <ProgressCircle
-                                        style={{ height: 90, width: 100 }}
-                                        progress={0.7}
+                                        style={{ height: 80, width: 80 }}
+                                        progress={deathPercentageWOW / 100}
                                         progressColor={'rgb(134, 65, 244)'}
-                                        startAngle={-Math.PI * 0.8}
-                                        endAngle={Math.PI * 0.8}
                                     >
                                         <View style={styles.progressCharts}>
-                                            <Text>25%</Text>
+                                            <Text>{`${deathPercentageWOW} %`}</Text>
                                         </View>
                                     </ProgressCircle>
 
                                 </View>
                                 <View style={styles.graficView}>
-                                <Text style={styles.text}>grafico</Text>
+                                    <Text style={styles.text}>confirmados %</Text>
+                                    <ProgressCircle
+                                        style={{ height: 80, width: 80 }}
+                                        progress={confirmedPercentage / 100}
+                                        progressColor={'rgb(134, 65, 244)'}
+                                    >
+                                        <View style={styles.progressCharts}>
+                                            <Text>{`${confirmedPercentage} %`}</Text>
+                                        </View>
+                                    </ProgressCircle>
+
+                                </View>
+                                {/* <View style={styles.graficView}>
+                                    <Text style={styles.text}>grafico</Text>
                                     <PieChart
                                         style={{ height: 90, width: 100 }}
                                         data={piechart}
                                     >
+                                        <Label />
                                     </PieChart>
-                                </View>
+                                </View> */}
                             </View>
                         </ScrollView>
                     </SafeAreaView>
@@ -134,33 +191,34 @@ const styles = StyleSheet.create({
         padding: 10
     },
     areChartsContainer: {
-        justifyContent: 'center', 
-        alignItems: 'center', 
+        justifyContent: 'center',
+        alignItems: 'center',
         marginTop: 15,
     },
     areChartsView: {
         flexDirection: 'row',
-         height: 200, 
-         width: 200,
+        flex: 1,
+        height: 200,
+        width: 200,
     },
     graficContainer: {
         flex: 1, flexDirection: 'row',
         marginTop: 15,
         justifyContent: 'space-around',
-     },
-     progressCharts: {
-        justifyContent: 'center', 
+    },
+    progressCharts: {
+        justifyContent: 'center',
         alignItems: 'center',
-     },
-     text: {
-     fontSize: 10,
-     },
-     graficView: {
-        width: 130, 
-        height: 150, 
+    },
+    text: {
+        fontSize: 10,
+    },
+    graficView: {
+        width: 130,
+        height: 150,
         borderRadius: 15,
-        backgroundColor: '#ccc', 
+        backgroundColor: '#fff',
         justifyContent: 'space-around',
         alignItems: 'center',
-     }
+    }
 });
